7b9abae948dddff63c5d9dbcd244b7e90b9dbfe0
Daniel Miranda <danielmr1914@gmail.com>
2024-11-14 12:23:33 -0600

    Categoria
    Creacion de clases
    Categoria
    CategoriaController
    CategoriaDao
    CategoriaService
    CategoriaServiceImpl

---
+++ src/main/java/com/TiendaRopa/service/CategoriaService.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.TiendaRopa.service;
+
+
+import com.TiendaRopa.domain.Categoria;
+import java.util.List;
+
+public interface CategoriaService {
+    public List<Categoria> getCategorias(boolean activos);
+    public Categoria getCategoria(Categoria categoria);
+    public void save(Categoria categoria);
+    public void delete(Categoria categoria);
+
+}
