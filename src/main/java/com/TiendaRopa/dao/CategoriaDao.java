df486196ce9d1afa9ce3cdef8cacf8601f8c5cc6
samorafi <sebasmora172522@gmail.com>
2024-11-14 13:30:22 -0600

    Correcion de errores

--- src/main/java/com/TiendaRopa/dao/CategoriaDao.java
+++ src/main/java/com/TiendaRopa/dao/CategoriaDao.java
@@ -1,13 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
+
 package com.TiendaRopa.dao;
 
-/**
- *
- * @author Jose Daniel
- */
-public interface CategoriaDao {
+import com.TiendaRopa.domain.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoriaDao extends JpaRepository <Categoria,Long>{
     
 }
